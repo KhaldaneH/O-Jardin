9340652c1f560152df750c2144c4a382145444c4
KhaldaneH <122120678+KhaldaneH@users.noreply.github.com>
2024-08-11 23:07:46 +0200

    Update Header3.jsx

--- frontend/src/components/Header/Header3/Header3.jsx
+++ frontend/src/components/Header/Header3/Header3.jsx
@@ -4,35 +4,13 @@ import {Link} from 'react-router-dom'
 
 
 const Header3 = () => {
-    useEffect(() => {
-        const backgrounds = [
-            '/hero-3.jpg',
-            '/hero-4.jpg',
-            '/hero-5.jpg'
-        ];
-
-        let currentIndex = 0;
-        const Header3 = document.querySelector('.header3');
-
-        // Set the initial background image immediately
-        Header3.style.backgroundImage = `url(${backgrounds[currentIndex]})`;
-
-        const changeBackground = () => {
-            currentIndex = (currentIndex + 1) % backgrounds.length;
-            Header3.style.backgroundImage = `url(${backgrounds[currentIndex]})`;
-        };
-
-        const intervalId = setInterval(changeBackground, 5000); // Change the image every 5000ms (5 seconds)
-
-        return () => clearInterval(intervalId); // Cleanup on component unmount
-    }, []);
-
+    
     return (
         <div className='header3'>
             <div className='header3-contents'>
-                <h2>Bienvenue au Café O Jardin</h2>
-                <p>Choisissez parmi un menu varié comprenant une gamme délicieuse de plats préparés avec les meilleurs ingrédients et l'expertise culinaire. Nous avons également un magasin, consultez-le</p>
-                <Link to="/menu"><button>Voir le Menu</button></Link>
+                <h2 style={{fontSize:"60px",marginBottom:"30px"}}>Menu</h2>
+                <p style={{marginBottom:"50px",}}>Au “Cafe O Jardin”, nous vous proposons une variété de plats délicieux et de boissons rafraîchissantes qui satisfont tous les goûts.</p>
+                <button>Télécharger le menu</button>
             </div>
         </div>
     );
