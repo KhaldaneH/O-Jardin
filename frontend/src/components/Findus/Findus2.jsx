4a633a29c111dd392237c9fa6f119ac5392aeecb
KhaldaneH <122120678+KhaldaneH@users.noreply.github.com>
2024-08-11 16:59:19 +0200

    Update Findus2.jsx

--- frontend/src/components/Findus/Findus2.jsx
+++ frontend/src/components/Findus/Findus2.jsx
@@ -20,7 +20,7 @@ const FindUs2 = () => {
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13756.38204558894!2d-8.8839535!3d30.4617327!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdb1737a1ee7b413%3A0xbd8adb4d5f5cc49d!2zw5QgSmFyZGluIENhZsOp!5e0!3m2!1sen!2sma!4v1723134228278!5m2!1sen!2sma"
             width="100%"
             height="300"
-            style={{ border: 0,width:'700px',height:'350px' }}
+            style={{ border: 0}}
             allowFullScreen=""
             loading="lazy"
           ></iframe>
